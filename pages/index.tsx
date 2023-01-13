931598ee691915fee42d34e2b0e8e06a620fda7a
web3sdks-template <developtemplate@web3sdks.com>
2023-01-13 11:08:24 +0800

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import { MediaRenderer, useContract, useNFTs } from "@web3sdks/react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const { contract } = useContract(
+    "0x05B8aab3fd77580C29c6510d8C54D9E6be4262d2"
+  );
+
+  const { data: nfts, isLoading } = useNFTs(contract, {
+    start: 0,
+    count: 10,
+  });
+
+  return (
+    <div className={styles.container}>
+      {isLoading && <div>Loading...</div>}
+      {nfts?.map((nft) => (
+        <MediaRenderer
+          key={nft.metadata.id}
+          src={nft.metadata.image}
+          alt={String(nft.metadata.name)}
+          style={{
+            maxWidth: "200px",
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
